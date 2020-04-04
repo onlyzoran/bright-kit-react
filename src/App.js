b81c101d00d8b4aceca10bdd2b9b6c8990e58c36
Zoran <onlyzoran@gmail.com>
2020-04-04 21:47:32 +0500

    Create Hello page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-page-wrapper">
+      <div className="main-page-inner">
+        <img src={logo} alt="logo" />
+        <h1 className="main-page-title">BRIGHT KIT</h1>
+      </div>
     </div>
   );
 }
